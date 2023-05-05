fae8a25d719bd302ac28028ad79334887190f89d
CohenK <kangcohen@gmail.com>
2023-05-05 14:13:49 -0700

    fixed path for previous test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/About.css";
-import tree from "/assets/tree.jpg";
+import tree from "../assets/tree.jpg";
 
 function About() {
   let hobbies: [string, string][] = [
