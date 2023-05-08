9082ccaba96eab98a0f3c1985b01bf876b164f0d
CohenK <CohenK@users.noreply.github.com>
2023-05-08 01:38:56 +0000

    deploy: 94a25befd4f45fff37894bc9721b2336e3eb2e0f

---
+++ assets/skills-40cd3700.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,R as s,A as o}from"./App-160d679b.js";t.createRoot(document.getElementById("root")).render(e.jsx(s.StrictMode,{children:e.jsx(o,{page:"skills"})}));
--- assets/skills-524499af.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,R as s,A as o}from"./App-d3c1b308.js";t.createRoot(document.getElementById("root")).render(e.jsx(s.StrictMode,{children:e.jsx(o,{page:"skills"})}));
