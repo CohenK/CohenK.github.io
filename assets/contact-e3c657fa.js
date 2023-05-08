872d2502063e71c3e2ee3fc03bbf6e7c01ca2cf5
CohenK <CohenK@users.noreply.github.com>
2023-05-08 00:54:42 +0000

    deploy: 5498f2589db593067a572d96e5710adccb804433

---
+++ assets/contact-e3c657fa.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o,A as c}from"./App-d3c1b308.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(c,{page:"contact"})}));
---
+++ assets/skills-524499af.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,R as s,A as o}from"./App-d3c1b308.js";t.createRoot(document.getElementById("root")).render(e.jsx(s.StrictMode,{children:e.jsx(o,{page:"skills"})}));
