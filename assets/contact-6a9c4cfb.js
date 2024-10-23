946b2a48b20532d209f11d3261a042298db3e2b3
CohenK <kangcohen@gmail.com>
2024-10-23 16:38:36 -0600

    Updates

--- assets/contact-6a9c4cfb.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,R as o,A as c}from"./App-622f2322.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(c,{page:"contact"})}));
--- assets/skills-21aa1ad3.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,R as s,A as o}from"./App-622f2322.js";t.createRoot(document.getElementById("root")).render(e.jsx(s.StrictMode,{children:e.jsx(o,{page:"skills"})}));
